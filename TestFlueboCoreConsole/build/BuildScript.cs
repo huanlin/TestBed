fc1b696b2e11f93a3c3d5bcdd44aa68594c58a79
Michael Tsai <huanlin.tsai@gmail.com>
2020-03-31 10:50:41 +0800

    Added example project for testing FlubuCore.

---
+++ TestFlueboCoreConsole/build/BuildScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FlubuCore.Context;
+using FlubuCore.Context.Attributes.BuildProperties;
+using FlubuCore.IO;
+using FlubuCore.Scripting;
+using FlubuCore.Tasks.Attributes;
+using FlubuCore.Tasks.Versioning;
+
+namespace build
+{
+    public class BuildScript : DefaultBuildScript
+    {
+        public FullPath OutputDir => RootDirectory.CombineWith("output");
+
+        [SolutionFileName]
+        public string SolutionFileName => RootDirectory.CombineWith("source/ConsoleApp1.sln");
+
+        [BuildConfiguration]
+        public string BuildConfiguration { get; set; } = "Release"; // Debug or Release
+
+        protected override void ConfigureBuildProperties(IBuildPropertiesContext context)
+        {
+            context.Properties.Set(BuildProps.ProductId, "ConsoleApp1");
+            context.Properties.Set(BuildProps.ProductName, "ConsoleApp1");
+        }
+
+        protected override void ConfigureTargets(ITaskContext session)
+        {
+            Console.WriteLine($"RootDirectory: {RootDirectory}");
+            Console.WriteLine($"OutputDir: {OutputDir}");
+
+            var target = session.CreateTarget("compile")
+                .SetDescription("Compile the solution.")
+                .AddCoreTask(x =>
+                    x.Build()
+                    .Output(OutputDir));         
+        }
+    }
+}
---
+++ TestFlueboCoreConsole/build/Program.cs
@@ -0,0 +1,16 @@
+﻿using FlubuCore.Scripting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new FlubuEngine();
+            engine.RunScript<BuildScript>(new string[] { "compile" });
+        }
+    }
+}
